723779bddb6eedd218aad85cf6bc6173d475d1c4
jeisongarcia9713 <jegarciat@correo.udistrital.edu.co>
2021-10-04 08:13:34 -0500

    the code to check n in the decryption has been upodated to check if n is product of two prime numbers

--- src/Decrypt.cpp
+++ src/Decrypt.cpp
@@ -4,7 +4,8 @@ using namespace std;
 using namespace scanner;
 using namespace rsa;
 
-namespace rsa{
+namespace rsa
+{
 
     Decrypt::Decrypt() : n(221), d(77) //default values to test impementation
     {
@@ -23,12 +24,12 @@ namespace rsa{
         decryptedData.clear();
         for (long &character : str2decode)
         {
-            
-            long c= modpow(character,d,n);
+
+            long c = modpow(character, d, n);
             /*Checking that the data that was decrypted has a representatión following the requeriments of the test*/
             if (c >= rsa::ENCR_A && c <= rsa::ENCR_Z)
             {
-                c = c + rsa::ASCII_A - 1; // convert to ascci value 
+                c = c + rsa::ASCII_A - 1; // convert to ascci value
             }
             else if (c == rsa::ENCR_SPACE)
             {
@@ -36,10 +37,10 @@ namespace rsa{
             }
             else
             {
-                cout << "Invalid data is triying to be decrypted with value:"<< c << endl;
+                cout << "Invalid data is triying to be decrypted with value:" << c << endl;
                 exit(0);
             }
-            decryptedData += (char) c;
+            decryptedData += (char)c;
         }
     }
 
@@ -48,16 +49,16 @@ namespace rsa{
         decryptedData.clear();
         for (long &character : str2decode)
         {
-            long c= modpow(character,d,n);
+            long c = modpow(character, d, n);
             try
             {
                 c = DECRYPTION_MAP.at(c);
-                decryptedData += (char) c;
+                decryptedData += (char)c;
             }
             catch (exception &e)
             {
                 /*Exception out of range indicates that there is no value for the entered key*/
-                cout << "Invalid data is triying to be decrypted with value:"<< c << endl;
+                cout << "Invalid data is triying to be decrypted with value:" << c << endl;
                 exit(0);
             }
         }
@@ -65,28 +66,39 @@ namespace rsa{
 
     bool Decrypt::checkD_NData()
     {
-        int errors=0;
-        if(d<1){
-            cout<< "d must greater than 0"<<endl;
+        int errors = 0;
+        if (d < 1)
+        {
+            cout << "d must greater than 0" << endl;
             errors++;
         }
-        if(n<221){
-            cout<< "n must be greater or equal to 221 "<<endl; //this having in consideration the guideline where P>=13 and Q>=17
+        if (n < 221)
+        {
+            cout << "n must be greater or equal to 221 " << endl; //this having in consideration the guideline where P>=13 and Q>=17
             errors++;
         }
-        
-        if(errors) return false;
+        else
+        {
+            long p = found_first_factor(n);
+            long q = n / p;
+            if (p != q && (!is_prime(p) || !is_prime(q,p)))
+            {
+                cout << "n is no the product of two prime numbers" << endl;
+                errors++;
+            }
+        }
+        if (errors)
+            return false;
         return true;
     }
-
 
     /*ostream Output stream objects can write sequences of characters and represent other kinds of data*/
     ostream &operator<<(ostream &os, const Decrypt &Encry)
     {
         if (!Encry.decryptedData.empty())
         {
-           
-            os <<Encry.decryptedData <<endl;
+
+            os << Encry.decryptedData << endl;
         }
         else
         {
@@ -121,30 +133,30 @@ namespace rsa{
     }
 }
 
-int main(){
-    
+int main()
+{
+
     long d = Scanner::scanData<long>("d", "long");
     long n = Scanner::scanData<long>("n", "long");
     Decrypt decr(n, d);
-    long p =found_first_factor(n);
-    long q =n/p; 
-    cout<<"first factor"<<p<<endl;
-    cout<<"second factor"<<q<<endl;
-    cout<<"first factor is prime"<<is_prime(p)<<endl;
-    cout<<"second factor is prime"<<is_prime(q)<<endl;
-    if(!decr.checkD_NData()) exit(0);
+    if (!decr.checkD_NData())
+        exit(0);
 
-    bool error=false;
+    bool error = false;
     vector<long> str2decode;
-    do{
-        try{
-            str2decode= Scanner::splitData(Scanner::scanData("Encrypted"));
-            error=false;
-        }catch(std::exception& e){
-            cout<<"invalid data must enter just numbers separate each them by commas"<<endl;
-            error=true;
+    do
+    {
+        try
+        {
+            str2decode = Scanner::splitData(Scanner::scanData("Encrypted"));
+            error = false;
         }
-    }while(error);
+        catch (std::exception &e)
+        {
+            cout << "invalid data must enter just numbers separate each them by commas" << endl;
+            error = true;
+        }
+    } while (error);
 
     decr.calcDecryption(str2decode);
     cout << "******************************" << endl;
@@ -158,5 +170,4 @@ int main(){
     cout << "The decrypted data using the map representation is : ";
     cout << decr;
     cout << "******************************" << endl;
-
 }
